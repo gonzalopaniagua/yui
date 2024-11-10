174bf3bcd6c454df0f9baa09cb9a4de7ddb50001
Gonsalico <gonzalo.paniagua.sds@gmail.com>
2024-11-10 20:26:23 +0100

    add https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://8n7vlqq7l4.execute-api.eu-central-1.amazonaws.com/prod/v1",
+  baseURL: "https://8n7vlqq7l4.execute-api.eu-central-1.amazonaws.com/prod/v1",
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,6 +12,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     // Handle errors here (e.g., refresh token, logout, etc.)
+    console.error("API Error:", error);
     return Promise.reject(error);
   }
 );
